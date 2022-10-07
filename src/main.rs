f5fefc9706c1875a64a2420eafd68e4f02b5c625
functional-tim <crazymind102@googlemail.com>
2022-10-07 03:20:07 +0200

    GUI testing

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
  */
 
 use exitcode;
+use eframe::egui;
 use heck::ToTitleCase;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
@@ -68,6 +69,7 @@ fn transform(st: &mut Vec<String>) {
 
 
 // Main program logic.
+/*
 fn main() {
     let mut wordlist: Vec<String> = include_str!("../12dicts/International/3of6game.txt").split('\n').map(|x| x.parse::<String>().unwrap()).collect();
     let opt = Opt::from_args();
@@ -84,4 +86,45 @@ fn main() {
     let password = password::create_password(&mut wordlist, opt.seperator1, opt.seperator2, opt.number);
     println!("{}", password);
     exit(exitcode::OK);
+}
+*/
+// GUI Stuff
+fn main() {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
 }
