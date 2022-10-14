5fd75c25c07d596447f9c8ae1b73490ed1c9f59f
functional-tim <crazymind102@googlemail.com>
2022-10-14 21:52:52 +0200

    further GUI testing

---
+++ src/easypassword.rs
@@ -0,0 +1,120 @@
+/*
+ * main.rs - Console program to create the password.
+ *
+ * (C) 2020 Tim Gravert <tim.gravert@web.de>
+ *
+ * License: MIT OR Apache-2.0
+ *
+ */
+
+use eframe::egui;
+use heck::ToTitleCase;
+//use std::fs::File;
+//use std::io::{prelude::*, BufReader};
+//use std::path::{Path, PathBuf};
+//use std::process::exit;
+//use structopt::StructOpt;
+
+mod password;
+
+// Auxiliary function to transform the input file into a Vector of single words.
+// Input file has to be formatted in such a way that every word is on a single line.
+/*
+fn lines_from_file(filename: impl AsRef<Path>) -> Result<Vec<String>, (String, i32)> {
+    let file = match File::open(filename) {
+        Ok(file) => file,
+        Err(_) => return Err((String::from("no such file"), exitcode::NOINPUT)),
+    };
+    let buf = BufReader::new(file);
+    match buf.lines().collect() {
+        Ok(res) => Ok(res),
+        Err(_) => Err((
+            String::from("file contained invalid UTF-8"),
+            exitcode::DATAERR,
+        )),
+    }
+}*/
+
+fn transform(st: &mut Vec<String>) {
+    for s in st {
+        s.retain(|c| !c.is_whitespace());
+        *s = s.to_title_case();
+    }
+}
+
+// GUI implementations
+struct MyApp {
+    wordlist: Vec<String>,
+    seperator1: String,
+    seperator2: String,
+    number: usize,
+    password: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            wordlist: {
+                let mut wordlist: Vec<String> =
+                    include_str!("../12dicts/International/3of6game.txt")
+                        .split('\n')
+                        .map(|x| x.parse::<String>().unwrap())
+                        .collect();
+                transform(&mut wordlist);
+                wordlist
+            },
+            seperator1: "%".to_owned(),
+            seperator2: "5".to_owned(),
+            number: 4,
+            password: "".to_owned(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("easypassword");
+            ui.horizontal(|ui| {
+                ui.label("Wordlist");
+                ui.text_edit_singleline(&mut "3of6game.txt");
+            });
+            ui.horizontal(|ui| {
+                ui.label("Seperator 1");
+                ui.text_edit_singleline(&mut self.seperator1);
+            });
+            ui.horizontal(|ui| {
+                ui.label("Seperator 2");
+                ui.text_edit_singleline(&mut self.seperator2);
+            });
+            ui.add(egui::Slider::new(&mut self.number, 4..=8).text("Number of words"));
+            if ui.button("Click to generatre password").clicked() {
+                self.password = password::create_password(
+                    &mut self.wordlist,
+                    self.seperator1.to_owned(),
+                    self.seperator2.to_owned(),
+                    self.number,
+                );
+            }
+            ui.horizontal(|ui| {
+                ui.label("Password");
+                ui.text_edit_singleline(&mut self.password);
+            });
+        });
+    }
+}
+
+// Main program logic.
+fn main() {
+    let mut wordlist: Vec<String> = include_str!("../12dicts/International/3of6game.txt")
+        .split('\n')
+        .map(|x| x.parse::<String>().unwrap())
+        .collect();
+    transform(&mut wordlist);
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "easypassword",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}
--- src/password.rs
+++ src/password.rs
@@ -10,7 +10,7 @@
 use rand::Rng;
 use std::mem::swap;
 
-/* 
+/*
  * Function to create a easy to remember password.
  * It takes the wordlist, two seperators and the number which is the number of words in the
  * password.
@@ -27,7 +27,7 @@ pub fn create_password(wordlist: &mut Vec<String>, s1: String, s2: String, n: us
  * password.
  *
  */
-pub fn password(words: Vec<String>, mut s1: String, mut s2: String) -> String {
+fn password(words: Vec<String>, mut s1: String, mut s2: String) -> String {
     let mut password = String::new();
     for mut word in words {
         word.push_str(&s1);
@@ -45,12 +45,10 @@ pub fn password(words: Vec<String>, mut s1: String, mut s2: String) -> String {
  * The first letter of each String gets capitalized to add to the security of the password.
  *
  */
-pub fn choose_words(words: &mut Vec<String>, n: usize) -> Vec<String> {
+fn choose_words(words: &mut Vec<String>, n: usize) -> Vec<String> {
     let mut rng = rand::thread_rng();
     let mut chosen: Vec<String> = Vec::with_capacity(n);
     //words = words.into_iter().map(|x| x.trim().to_string()).collect::<Vec<String>>();
-    chosen.resize_with(n, || {
-        words.remove(rng.gen_range(0..words.len()))
-    });
+    chosen.resize_with(n, || words.remove(rng.gen_range(0..words.len())));
     chosen
 }
